c77d9b04ff3123dada9c2a35fa8eba4c5a8c1f47
IDdev33 <127314323+IDdev33@users.noreply.github.com>
2024-01-10 23:49:18 +0200

    Fixes 7

--- MusicBlog/settings.py
+++ MusicBlog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-bu3c4zdj$5uxrfw)mt(0i62z=pj@-7if6=$28s(hg7qx=v*x32'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'my-music-blog.onrender.com']
 
