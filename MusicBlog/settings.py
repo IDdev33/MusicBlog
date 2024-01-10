29142210fe7fa0a165633f712d1d1120ae2ff52a
IDdev33 <127314323+IDdev33@users.noreply.github.com>
2024-01-10 23:37:16 +0200

    Minor fixes 5

--- MusicBlog/settings.py
+++ MusicBlog/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-bu3c4zdj$5uxrfw)mt(0i62z=pj@-7if6=$28s(hg7qx=v*x32'
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-bu3c4zdj$5uxrfw)mt(0i62z=pj@-7if6=$28s(hg7qx=v*x32')
+SECRET_KEY = 'django-insecure-bu3c4zdj$5uxrfw)mt(0i62z=pj@-7if6=$28s(hg7qx=v*x32'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True')=='True'
 
